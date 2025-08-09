b0023d873f7a738fda623782ebfc1233b3884ad9
brekk <brekk@brekkbockrath.com>
2025-08-09 13:39:52 -0700

    homepage wired for sound

--- app/page.js
+++ app/page.js
@@ -3,18 +3,19 @@ import { useState, useEffect } from "react"
 import blem from "blem"
 
 import LinkedList from "#/components/LinkedList"
-import { div as Biv } from "#/components/Bemoan"
+import { blemish } from "#/components/Bemoan"
 import Logo from "#/components/EsaLogo"
 import { LINKS, FOOTER_LINKS } from "#/HARDCODED_GARBAGE"
 import Heading from "#/components/Heading"
 import Page from "#/components/Page"
 import { useAPI } from '#/utilities/fetch'
 
-import { bem } from "#/utilities/style"
+const bem = blem('Homepage')
+const Biv = blemish(bem)
+import '#/app/page.scss'
 
 const App = () => {
   const $page = useAPI('/api/homepage')
-  console.log("$PAGE", $page)
   return (
     <Page $data={$page.data}>
       {$page?.data ? (
@@ -25,9 +26,12 @@ const App = () => {
       <h2 className={bem('heading', 'h2')}>
         {$page.data.subtitle}
       </h2>
+      <Biv e="content">
+      <img src={$page.data.image} className={bem('image')} />
       <p className={bem('cta')}>
         {$page.data.text}
       </p>
+      </Biv>
       </>
       ) : null}
     </Page>
