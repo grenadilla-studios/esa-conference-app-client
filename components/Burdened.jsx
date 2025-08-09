6481a07363cc1db9b6ef22d2a67d2b65d5d544b2
brekk <brekk@brekkbockrath.com>
2025-08-09 09:11:55 -0700

    debuggery

--- components/Burdened.jsx
+++ components/Burdened.jsx
@@ -1,10 +1,20 @@
-export const Burden = ({title, load}) => (
-  <details>
-    <summary>{title}</summary>
-    <pre>{JSON.stringify(load, null, 2)}</pre>
+import blem from "blem"
+import "#/components/Debug.scss"
+
+const bem = blem("Data")
+
+export const Burden = ({ title, className: clx, load }) => (
+  <details className={bem("details")}>
+    <summary className={bem("summary")}>{title}</summary>
+    <pre className={bem("body")}>{JSON.stringify(load, null, 2)}</pre>
   </details>
 )
 
-export const Burdened = ({children}) => <><Burden {...children[0]} />{children[1]}</>
+export const Burdened = ({ children, className }) => (
+  <>
+    <Burden {...children[0]} />
+    {children[1]}
+  </>
+)
 
 export default Burdened
